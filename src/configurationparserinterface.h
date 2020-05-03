0f2bdc7a2749a368c0470671235d1155fc75b911
Slav <ivanov.slk@abv.bg>
2020-05-03 12:54:00 +0300

    added configuration parser interface

--- src/configurationparserinterface.h
+++ src/configurationparserinterface.h
@@ -1,7 +1,18 @@
+/**
+ * Parses a configuration file.
+*/
 class ConfigurationParserInterface
 {
 public:
     virtual ~ConfigurationParserInterface() = default;
+
+    /**
+     * Parses the configuration file.
+     */
     virtual void read_config_file();
+
+    /**
+     * Returns an object representing the parsed configuration
+     */
     virtual void create_configuration();
 };
