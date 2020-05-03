71c5c02f6860129116ed7e4adab0189e78851477
Slav <ivanov.slk@abv.bg>
2020-05-03 17:03:00 +0300

    updated compiler versions

--- .git-hooks/run_build.sh
+++ .git-hooks/run_build.sh
@@ -6,6 +6,7 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "${DIR}/../build"
 echo "BUILD: in "$(pwd)
 # rm -r *
-cmake ..
+
+cmake -D CMAKE_C_COMPILER=/usr/bin/gcc-9 -D CMAKE_CXX_COMPILER=/usr/bin/g++-9 ..
 make
 test/run_tests
