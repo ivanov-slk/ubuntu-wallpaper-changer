a849aa4cf48ac9a829918ebf562c94af5bf50890
Slav <ivanov.slk@abv.bg>
2020-05-06 21:37:33 +0300

    added structure for the directory handler

---
+++ src/dirhandler.cpp
@@ -0,0 +1,8 @@
+/**
+ * This class has a directory path field. It is responsible
+ * for initializing a root directory, the file selection strategy
+ * and the execution of the latter. 
+ */
+class DirHandler
+{
+};
