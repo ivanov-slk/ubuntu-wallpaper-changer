98ce16c17e9d682f865310b29220b2cf6781c0b0
Slav <ivanov.slk@abv.bg>
2020-05-10 13:24:08 +0300

    added SIGINT and SIGTERM handling

--- main.cpp
+++ main.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
 #include <unistd.h>
 #include <string>
+#include <csignal>
 
 #include <appfacade.cpp>
 
 using namespace std;
+
+void signal_handler(int signal)
+// Useful (and used) only for SIGINT and SIGTERM. If other signals need
+// to be caught, std::exit might not be the best course of action.
+{
+    cout << "Signal caught: " << signal << ". Exiting..." << '\n';
+    exit(signal);
+}
 
 int main()
 {
     // ChangeCommand system_command{"gsettings set org.gnome.desktop.background picture-uri file://",
     //                              "/home/slav/Изображения/Wallpapers/1 (2).jpg"};
     // system_command.execute();
+    signal(SIGINT, signal_handler);
+    signal(SIGTERM, signal_handler);
     AppFacade wallpaper_changer{"test/resources/real_config.cfg"};
-    for (int i = 0; i < 5; i++)
+    while (true)
     {
         string command_line = wallpaper_changer.change_wallpaper();
         cout << command_line << '\n';
