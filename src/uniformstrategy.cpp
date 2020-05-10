c45e9b6a95ba85f6edbce9b83145a7c9ab1e9e78
Slav <ivanov.slk@abv.bg>
2020-05-10 21:51:05 +0300

    changes to to-do

--- src/uniformstrategy.cpp
+++ src/uniformstrategy.cpp
@@ -8,17 +8,28 @@
  */
 class UniformStrategy : public StrategyInterface
 {
-public:
-    std::vector<std::filesystem::path> execute(Directory root_dir) const override
+private:
+    std::vector<std::filesystem::path> cached_files;
+    std::vector<std::filesystem::path> traverse_get_files(Directory root_dir)
     {
         std::vector<std::filesystem::path> all_files = root_dir.get_files();
         std::vector<std::filesystem::path> all_subdirs = root_dir.get_subdirs();
         for (auto &sub_dir : all_subdirs)
         {
-            std::vector<std::filesystem::path> subdir_files = execute(Directory{sub_dir});
+            std::vector<std::filesystem::path> subdir_files = traverse_get_files(Directory{sub_dir});
             all_files.reserve(all_files.size() + subdir_files.size());
             all_files.insert(all_files.end(), subdir_files.begin(), subdir_files.end());
         }
         return all_files;
+    }
+
+public:
+    std::vector<std::filesystem::path> execute(Directory root_dir) override
+    {
+        if (!cached_files.empty())
+        {
+            return cached_files;
+        }
+        return traverse_get_files(root_dir);
     }
 };
