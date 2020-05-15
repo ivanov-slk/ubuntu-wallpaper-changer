af0c4596e8eb637fe6008371b11cdf052ed28228
Slav <ivanov.slk@abv.bg>
2020-05-15 22:25:10 +0300

    implemented change seconds

--- src/changecommand.cpp
+++ src/changecommand.cpp
@@ -1,4 +1,7 @@
 #pragma once
+#include <filesystem>
+#include <vector>
+#include <iostream>
 #include "commandinterface.h"
 
 class ChangeCommand : public CommandInterface
@@ -7,15 +10,50 @@ private:
     std::string filename;
     std::string command_template;
     std::string command_line;
+    std::vector<std::pair<std::string, int>> dir_seconds;
+    int default_seconds;
     ChangeParameters change_params;
+
+    int calculate_seconds(std::string check_path)
+    {
+        if (!dir_seconds.empty())
+        {
+            std::filesystem::path check_path{filename};
+            while (check_path != check_path.root_path())
+            {
+                std::string parent_dir = check_path.filename();
+                for (const std::pair<std::string, int> &dir_pair : dir_seconds)
+                {
+                    if (parent_dir == dir_pair.first)
+                    {
+                        std::cout << "+++++++++++++++++++++++++++++++++++++" << '\n';
+                        return dir_pair.second;
+                    }
+                }
+                check_path = check_path.parent_path();
+            }
+        }
+        return default_seconds;
+    }
 
 public:
     ChangeCommand() = default;
     ChangeCommand(const std::string &command_template, const std::string &filename) : command_template(command_template), filename(filename){};
+    ChangeCommand(const std::string &command_template,
+                  const std::string &filename,
+                  const std::vector<std::pair<std::string, int>> dir_seconds,
+                  int default_seconds) : command_template(command_template),
+                                         filename(filename),
+                                         dir_seconds(dir_seconds),
+                                         default_seconds(default_seconds){};
     void execute() override
     {
+        // get the command line
         command_line = command_template + "\"file://" + filename + "\"";
         change_params.command_line = command_line;
+
+        // get the change seconds
+        change_params.change_seconds = calculate_seconds(filename);
     };
     ChangeParameters get_change() { return change_params; };
 };
--- test/test_changecommand.cpp
+++ test/test_changecommand.cpp
@@ -25,3 +25,51 @@ TEST(ChangeCommandTests, ReturnsCommand2)
     ChangeParameters result = testable.get_change();
     ASSERT_EQ("gsettings set org.gnome.desktop.background picture-uri \"file:///this/path/does/not/exist/1 (2).jpg\"", result.command_line);
 }
+
+TEST(ChangeCommandTests, ReturnsSeconds)
+{
+    std::vector<std::pair<std::string, int>> change_seconds_vector{{"dir1", 10}, {"dir2", 20}, {"exist", 5}};
+    ChangeCommand testable{"gsettings set org.gnome.desktop.background picture-uri ",
+                           "/this/path/does/not/exist/1 (2).jpg",
+                           change_seconds_vector,
+                           30};
+    testable.execute();
+    ChangeParameters result = testable.get_change();
+    ASSERT_EQ(5, result.change_seconds);
+}
+
+TEST(ChangeCommandTests, ReturnsSecondsParent)
+{
+    std::vector<std::pair<std::string, int>> change_seconds_vector{{"dir1", 10}, {"dir2", 20}, {"not", 5}};
+    ChangeCommand testable{"gsettings set org.gnome.desktop.background picture-uri ",
+                           "/this/path/does/not/exist/1 (2).jpg",
+                           change_seconds_vector,
+                           30};
+    testable.execute();
+    ChangeParameters result = testable.get_change();
+    ASSERT_EQ(5, result.change_seconds);
+}
+
+TEST(ChangeCommandTests, ReturnsSecondsClosestParent)
+{
+    std::vector<std::pair<std::string, int>> change_seconds_vector{{"dir1", 10}, {"not", 20}, {"does", 5}};
+    ChangeCommand testable{"gsettings set org.gnome.desktop.background picture-uri ",
+                           "/this/path/does/not/exist/1 (2).jpg",
+                           change_seconds_vector,
+                           30};
+    testable.execute();
+    ChangeParameters result = testable.get_change();
+    ASSERT_EQ(20, result.change_seconds);
+}
+
+TEST(ChangeCommandTests, ReturnsSecondsDefault)
+{
+    std::vector<std::pair<std::string, int>> change_seconds_vector{{"dir1", 10}, {"dir2", 20}, {"dir3", 5}};
+    ChangeCommand testable{"gsettings set org.gnome.desktop.background picture-uri ",
+                           "/this/path/does/not/exist/1 (2).jpg",
+                           change_seconds_vector,
+                           30};
+    testable.execute();
+    ChangeParameters result = testable.get_change();
+    ASSERT_EQ(30, result.change_seconds);
+}
