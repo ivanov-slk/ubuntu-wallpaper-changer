db1bc9f138a873296cbdf66802a1e02c5e2944e8
Slav <ivanov.slk@abv.bg>
2020-05-15 14:25:22 +0300

    tests for directory exclusions and allowed file formats

--- test/test_directory.cpp
+++ test/test_directory.cpp
@@ -9,7 +9,7 @@ TEST(DirectoryTests, ReturnsRootPath)
     ASSERT_TRUE(result == correct);
 }
 
-TEST(DirectoryTests, ReturnsFiles)
+TEST(DirectoryTests, ReturnsDirectories)
 {
     Directory testable{"../test/resources/dir_tests"}; // assumes we are in build/
     std::vector<std::filesystem::path> result = testable.get_subdirs();
@@ -25,7 +25,34 @@ TEST(DirectoryTests, ReturnsFiles)
     ASSERT_TRUE(result == correct);
 }
 
-TEST(DirectoryTests, ReturnsDirectories)
+TEST(DirectoryTests, ReturnsDirectoriesWithExclusions)
+{
+    std::vector<std::string> exclusions{"test_dir3"};
+    std::vector<std::string> allowed_extensions;
+    Directory testable{"../test/resources/dir_tests", exclusions, allowed_extensions}; // assumes we are in build/
+    std::vector<std::filesystem::path> result = testable.get_subdirs();
+    std::vector<std::filesystem::path> correct{
+        /** 
+         * Filesystem iterator returns non-sorted (alphabetically) values.
+         * Here, it is not a problem, since alphabetical order does not
+         * matter, but consider sorting if needed.
+         */
+        "../test/resources/dir_tests/test_dir1",
+        "../test/resources/dir_tests/test_dir2"};
+    ASSERT_TRUE(result == correct);
+}
+
+TEST(DirectoryTests, ReturnsDirectoriesWithExclusionsEmpty)
+{
+    std::vector<std::string> exclusions{"test_dir3", "test_dir1", "test_dir2"};
+    std::vector<std::string> allowed_extensions;
+    Directory testable{"../test/resources/dir_tests", exclusions, allowed_extensions}; // assumes we are in build/
+    std::vector<std::filesystem::path> result = testable.get_subdirs();
+    std::vector<std::filesystem::path> correct;
+    ASSERT_TRUE(result == correct);
+}
+
+TEST(DirectoryTests, ReturnsFiles)
 {
     Directory testable{"../test/resources/dir_tests"}; // assumes we are in build/
     std::vector<std::filesystem::path> result = testable.get_files();
@@ -35,10 +62,38 @@ TEST(DirectoryTests, ReturnsDirectories)
          * Here, it is not a problem, since alphabetical order does not
          * matter, but consider sorting if needed.
          */
-        "../test/resources/dir_tests/file3.txt",
+        "../test/resources/dir_tests/file3.jpeg",
+        "../test/resources/dir_tests/file2.png",
         "../test/resources/dir_tests/file1.txt",
-        "../test/resources/dir_tests/file2.txt",
         "../test/resources/dir_tests/file4.txt"};
+    ASSERT_TRUE(result == correct);
+}
+
+TEST(DirectoryTests, ReturnsFilesAllowedExtensions)
+{
+    std::vector<std::string> allowed_extensions{".png", ".jpeg"};
+    std::vector<std::string> exclusions;
+    Directory testable{"../test/resources/dir_tests", exclusions, allowed_extensions}; // assumes we are in build/
+    std::vector<std::filesystem::path> result = testable.get_files();
+    std::vector<std::filesystem::path> correct{
+        /** 
+         * Filesystem iterator returns non-sorted (alphabetically) values.
+         * Here, it is not a problem, since alphabetical order does not
+         * matter, but consider sorting if needed.
+         */
+        "../test/resources/dir_tests/file3.jpeg",
+        "../test/resources/dir_tests/file2.png",
+    };
+    ASSERT_TRUE(result == correct);
+}
+
+TEST(DirectoryTests, ReturnsFilesAllowedExtensionsEmpty)
+{
+    std::vector<std::string> allowed_extensions{".fake", ".none"};
+    std::vector<std::string> exclusions;
+    Directory testable{"../test/resources/dir_tests", exclusions, allowed_extensions}; // assumes we are in build/
+    std::vector<std::filesystem::path> result = testable.get_files();
+    std::vector<std::filesystem::path> correct;
     ASSERT_TRUE(result == correct);
 }
 
