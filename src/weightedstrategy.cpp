604fa81c50ec86a1485cdaec11696b8d69a34f73
Slav <ivanov.slk@abv.bg>
2020-05-19 10:52:05 +0300

    weighted strategy tested and implemented

--- src/weightedstrategy.cpp
+++ src/weightedstrategy.cpp
@@ -1,6 +1,8 @@
 #pragma once
 #include <random>
+#include <numeric>
 #include "strategyinterface.h"
+#include "filepicker.cpp"
 #include "directory.cpp"
 
 /**
@@ -10,10 +12,10 @@
 class WeightedStrategy : public StrategyInterface
 {
 private:
-    std::vector<std::pair<std::string, int>> directory_priorities;
+    std::vector<std::pair<std::filesystem::path, int>> directory_priorities;
 
 public:
-    WeightedStrategy(const std::vector<std::pair<std::string, int>> &directory_priorities)
+    WeightedStrategy(const std::vector<std::pair<std::filesystem::path, int>> &directory_priorities)
         : directory_priorities(directory_priorities) {}
 
     /**
@@ -57,55 +59,120 @@ public:
     }
 
     /**
-     * @brief Normalizes the priorities, so that they sum to 1 (i.e. converts them to
-     * a probability distribution).
+     * @brief Normalizes the priorities, so that they represent a cumulative distribution function.
      * 
      * @param const std::vector<std::pair<std::string, int>> subdirs_with_priorities - a vector of
      * directory-priority pairs;
      * 
      * @return std::vector<std::pair<std::string, float>> - a vector of directory-priority pairs,
      * with the priorities summing to 1.
+     * 
+     * The basic steps are the following:
+     * 1. Divide each priority by the sum of all priorities
+     * (results in a probability mass function, i.e. gives the probability that each pair will be chosen);
+     * 2. Take the cumulative sum for each priority -
+     * 3. I.e. if the initial priorities are 50, 20, 30:
+     * - step 1 results in 0.5, 0.2, 0.3;
+     * - the values are sorted - 0.2, 0.3, 0.5;
+     * - step 2 results in 0.2, 0.5, 1.0
+     * 4. This means that the probability of getting the directory with initial priority 50 is still 50%,
+     * and the probability of getting the directory with initial priority 30 is still 30%.
      */
     std::vector<std::pair<std::filesystem::path, float>>
     normalize_priorities(const std::vector<std::pair<std::filesystem::path, int>> subdirs_with_priorities)
     {
         std::vector<std::pair<std::filesystem::path, float>> normalized_priorities;
+
+        // get the sum of the priorities
         int priority_sum = 0;
         for (auto &element : subdirs_with_priorities)
         {
             priority_sum += element.second;
         }
+
+        // get the share of each priority in the total sum (i.e. make them sum to one; i.e. make this a probability mass function)
         for (auto &element : subdirs_with_priorities)
         {
             normalized_priorities.push_back({element.first, (float)element.second / (float)priority_sum});
         }
-        std::sort(normalized_priorities.begin(),
-                  normalized_priorities.end(),
-                  [](auto &left, auto &right) { return left.second < right.second; });
+
+        // take the PMF from above and convert it to cumulative distribution function; works in-place
+        std::partial_sum(normalized_priorities.begin(),
+                         normalized_priorities.end(),
+                         normalized_priorities.begin(),
+                         [](const std::pair<std::filesystem::path, float> &x,
+                            const std::pair<std::filesystem::path, float> &y) {
+                             return std::pair<std::filesystem::path, float>{y.first, x.second + y.second};
+                         });
         return normalized_priorities;
     }
 
-    std::vector<std::filesystem::path> execute(Directory root_dir) override
+    /**
+     * @brief Picks a path from vector of filesystem::path at random, given the weights.
+     * 
+     * @param std::vector<std::pair<std::filesystem::path, float>> normalized_priorities - As per normalize_priorities() output;
+     * 
+     * @return std::filesystem::path
+     * 
+     * Expects that the input vector is sorted by priority, smallest first. This should be provided by
+     * default by normalize_priorities() (although, formally, the method is public and could be used anywhere);
+     * no explicit enforcement here.
+     * 
+     * An idea to make it more general is to add 1. sorting of the input vector at the beginning and 2. taking
+     * a random uniform number only in the range (from min to max) of the pair's second element.
+     */
+    std::filesystem::path pick_directory(std::vector<std::pair<std::filesystem::path, float>> normalized_priorities,
+                                         bool testing)
     {
-        // draw random number
-        std::random_device rd;
-        std::mt19937_64 gen(rd());
-        std::uniform_real_distribution<> dis(0.0, 1.0);
-        double threshold = dis(gen);
+        // set random generator for the uniform distribution, or fix at 0.5 for testing purposes
+        double threshold = 0.5;
+        if (!testing)
+        {
+            std::random_device rd;
+            std::mt19937_64 gen(rd());
+            std::uniform_real_distribution<> dis(0.0, 1.0);
+            threshold = dis(gen);
+        }
+        // extract the first directory that has priority less than or equal to `threshold`
+        std::vector<std::pair<std::filesystem::path, float>> picked_paths;
+        std::pair<std::string, float> check_equal_priority_pair;
+        for (auto &dir_pair : normalized_priorities)
+        {
+            if (threshold <= dir_pair.second && picked_paths.empty())
+            {
+                return dir_pair.first;
+            }
+        }
+        return std::filesystem::path{};
+    }
 
-        // get subfolders (vector of filesystem::path) + add root_dir.get_root_path() if there are valid files in it
+    std::vector<std::filesystem::path>
+    execute(Directory root_dir) override
+    {
         std::vector<std::filesystem::path> subdirs = root_dir.get_subdirs();
+        if (subdirs.empty())
+        {
+            return root_dir.get_files();
+        }
+
         if (root_dir.get_files().size() > 0)
         {
             subdirs.push_back(root_dir.get_root_path());
         }
-
-        // normalize priorities (before normalization add unmentioned directories + current root path)
         std::vector<std::pair<std::filesystem::path, int>> subdirs_with_priorities =
             create_subdirs_with_priorities(subdirs);
 
-        // loop over the subfolders and get the first folder with priority less than the random number
+        std::vector<std::pair<std::filesystem::path, float>> normalized_priorities = normalize_priorities(subdirs_with_priorities);
 
-        // recursively call the method until no subdirectories exist, or current root dir is selected
+        // loop over the subfolders and get the first folder with priority less than the random number
+        std::filesystem::path picked_subdir = pick_directory(normalized_priorities, false);
+        // We have picked root_dir, so just return the files in it.
+        // Otherwise, we would be just repeating the whole process over directories we have already checked.
+        // And potentially just repeat selecting the root path and checking over and over again its subfolders.
+        if (picked_subdir == root_dir.get_root_path())
+        {
+            return root_dir.get_files();
+        }
+        return execute(Directory{picked_subdir});
     }
 };
