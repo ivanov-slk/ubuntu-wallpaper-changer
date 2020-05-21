7f48bd92ac74b436e47c19fdecafa2511464f9ec
Slav <ivanov.slk@abv.bg>
2020-05-21 21:19:50 +0300

    appfacade now logs exceptions

--- main.cpp
+++ main.cpp
@@ -2,13 +2,11 @@
 
 #include <appfacade.cpp>
 
-using namespace std;
-
 void signal_handler(int signal)
 // Useful (and used) only for SIGINT and SIGTERM. If other signals need
 // to be caught, std::exit might not be the best course of action. (?)
 {
-    cout << "Signal caught: " << signal << ". Exiting..." << '\n';
+    std::cout << "Signal caught: " << signal << ". Exiting..." << '\n';
     exit(signal);
 }
 
@@ -24,12 +22,15 @@ int main()
     }
     catch (const std::exception &exc)
     {
+        std::cout << "=============== Exception caught. ===============" << '\n';
+        std::cout << exc.what() << '\n';
         wallpaper_changer.log("=============== Exception caught. ===============");
         wallpaper_changer.log(exc.what());
         throw;
     }
     catch (...)
     {
+        std::cout << "========== Unknown exception caught. ==========" << '\n';
         wallpaper_changer.log("========== Unknown exception caught. ==========");
         throw;
     }
