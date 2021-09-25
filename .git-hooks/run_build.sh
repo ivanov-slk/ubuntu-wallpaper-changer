56567ea1e9918b781d930f51ae6abc364f75b195
Slav Ivanov <ivanov.slk@abv.bg>
2021-09-25 19:06:06 +0300

    updated the hooks

--- .git-hooks/run_build.sh
+++ .git-hooks/run_build.sh
@@ -1,12 +1,17 @@
 #! /usr/bin/bash
-echo "BUILD: Starting build..."
+start_time=$(date +%s)
+echo "$(printf '%(%Y-%m-%d %H:%M:%S)T\n' -1) [BUILD] Starting build..."
 set -e
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "${DIR}/../build"
-echo "BUILD: in "$(pwd)
+echo "$(printf '%(%Y-%m-%d %H:%M:%S)T\n' -1) [BUILD] in $(pwd)"
 # rm -r *
 
-cmake -D CMAKE_C_COMPILER=/usr/bin/gcc-9 -D CMAKE_CXX_COMPILER=/usr/bin/g++-9 ..
-make
+echo "$(printf '%(%Y-%m-%d %H:%M:%S)T\n' -1) [BUILD] Compiling..."
+cmake -D CMAKE_C_COMPILER=/usr/bin/gcc-10 -D CMAKE_CXX_COMPILER=/usr/bin/g++-10 ..
+make -j 3
+echo -e "\n$(printf '%(%Y-%m-%d %H:%M:%S)T\n' -1) [BUILD] Running tests..."
 test/run_tests
+end_time=$(date +%s)
+echo -e "\n$(printf '%(%Y-%m-%d %H:%M:%S)T\n' -1) [BUILD] Build finished for $(($end_time-$start_time)) seconds."
